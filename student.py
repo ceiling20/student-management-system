226c3075f596ae4d1cb9b65c4610f91d4f98f598
ceiling20 <1179845317@qq.com>
2026-04-28 22:26:34 +0800

    Implement student score sorting feature
    Added functionality to sort students by score.

--- student.py
+++ student.py
@@ -16,6 +16,7 @@ while True:
     print("2.增加学生")
     print("3.退出")
     print("4.删除学生")
+    print("5.排序成绩")
     choice = input("请选择：")
     if choice == "1":
         for s in students:
@@ -39,6 +40,12 @@ while True:
             break
         else:
             print("没有此同学")
+    elif choice == "5":
+        print("排序成绩中")
+        student_sort = sorted(students,key = lambda x:x["score"],reverse=True)
+        for s in student_sort:
+            print(f"学生:{s["name"]},成绩:{s["score"]}")
+        print("排序完成")
     else:
         print("错误选项")
         break
